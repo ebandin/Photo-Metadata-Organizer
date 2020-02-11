a97950181884db670a7d5ada09877ef0d77c0a8d
Eric Bandin <ericmbandin@gmail.com>
2020-02-11 14:28:58 -0800

    Starting to integrate with new PhotoField database

---
+++ Photo Metadata Organizer/Models/PhotoModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace Photo_Metadata_Organizer.Models
+{
+    public class Photo
+    {
+        public int ID { get; set; }
+        private static int nextId = 1;
+
+        public string Name { get; set; }
+        public DateTime DateTime { get; set; }
+        public Aperature Aperature { get; set; }
+        public ISO ISO { get; set; }
+        public FocalLength FocalLength { get; set; }
+
+        public Photo()
+        {
+            ID = nextId;
+            nextId++;
+        }
+
+    }
+}
---
+++ Photo Metadata Organizer/ViewModels/BaseViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Photo_Metadata_Organizer.Models;
+
+namespace Photo_Metadata_Organizer.ViewModels
+{
+    public class BaseViewModel
+    {
+        // All columns, for display
+        public List<PhotoField> Columns { get; set; }
+
+        // The column to search, defaults to all
+        public PhotoField Column { get; set; } = PhotoField.All;
+
+        // View title 
+        public string Title { get; set; } = "";
+
+        public BaseViewModel()
+        {
+            Columns = new List<PhotoField>();
+
+            foreach (PhotoField enumVal in Enum.GetValues(typeof(PhotoField)))
+            {
+                Columns.Add(enumVal);
+            }
+        }
+    }
+}
