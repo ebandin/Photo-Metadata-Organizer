a38b5e5a7cefa67aaec72f89d498c44b728491e6
Eric Bandin <ericmbandin@gmail.com>
2020-02-11 18:21:27 -0800

    Added PHoto field type enumerator so it knows the colums avail.

---
+++ Photo Metadata Organizer/Models/PhotoFieldType.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Photo_Metadata_Organizer.Models
+{
+    public enum PhotoFieldType
+    {
+        // Enum representing the JobField properties of a Job
+        // that can be viewed and searched.
+
+        PhotoName,
+        DateTime,
+        Aperature,
+        Exposure,
+        ISO,
+        FocalLength,
+        All
+    }
+}
+
+
+
+
+
+    
+    
+    
